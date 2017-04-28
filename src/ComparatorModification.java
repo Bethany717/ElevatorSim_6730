0ec6a11463fc3aab06a647d431b128e5a90ce008
Bethany717 <wangbeishen2016@gmail.com>
2017-04-28 16:02:24 -0400

    clear

--- src/ComparatorModification.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorModification implements  Comparator<Request>{
-	public int compare(Request r1, Request r2){
-		return r1.getEndingFloor()-r2.getEndingFloor();
-	}
-}
--- src/ComparatorModification_.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorModification_ implements  Comparator<Request>{
-	public int compare(Request r1, Request r2){
-		return r2.getEndingFloor()-r1.getEndingFloor();
-	}
-}
